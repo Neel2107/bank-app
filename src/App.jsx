a4335cf16879f36fdfd84eaa00e7addbe749cb85
Neel <neelpatel805804@gmail.com>
2022-12-06 04:21:36 +0530

    Nav Completed, Hero About to Complete

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,9 @@ const App = () => (
     </div>
 
     <div className={`bg-primary ${`styles.flexStart`} `}>
-      <div className={`${styles.boxWidth}`}><Hero/></div>
+      <div className={`${styles.boxWidth}`}>
+        <Hero/>
+        </div>
     </div>
     <div className={`bg-primary ${styles.paddingX} ${`styles.flexStart`} `}>
       <div className={`${styles.boxWidth}`}>

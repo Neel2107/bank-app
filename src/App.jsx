910ecf67f78580180b17c62da53f8fedd18eab04
Neel <neelpatel805804@gmail.com>
2022-12-07 00:21:17 +0530

    Final Commit of Front-end

--- src/App.jsx
+++ src/App.jsx
@@ -19,12 +19,12 @@ const App = () => (
       <div className={`${styles.boxWidth}`}><Navbar/></div>
     </div>
 
-    <div className={`bg-primary ${`styles.flexStart`} `}>
+    <div className={`bg-primary ${styles.flexStart} `}>
       <div className={`${styles.boxWidth}`}>
         <Hero/>
         </div>
     </div>
-    <div className={`bg-primary ${styles.paddingX} ${`styles.flexStart`} `}>
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart} `}>
       <div className={`${styles.boxWidth}`}>
         <Stats/> <Business/> <Billing/> <CardDeal/> <Testimonials/> <Clients/> <CTA/> <Footer/>
       </div>
